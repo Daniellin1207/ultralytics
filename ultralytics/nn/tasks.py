22eb18274666f0f22ee9c0d92355243c362aa90a
Daniel Lin <linyili1224@163.com>
2024-09-20 12:07:49 +0800

    对CBAM等注意力机制层改写适应yolo-s的宽度变化

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -982,13 +982,18 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             args = [c1, c2, *args[1:]]
         elif m is CBFuse:
             c2 = ch[f[-1]]
-        elif m in {CBAM}:
+        elif m in {CBAM,SEAttention,CoordAttention}:
             c1, c2 = ch[f], args[0]
             if c2 != nc:
                 c2 = make_divisible(min(c2, max_channels) * width, 8)
             args = [c1, *args[1:]]
-        elif m in {SEAttention}:
-            c2 = ch[f] # 为了引用下SEAttention
+        # elif m in {SEAttention,CoordAttention}:
+        #
+        #     c1, c2 = ch[f], args[0]
+        #     if c2 != nc:  # if c2 not equal to number of classes (i.e. for Classify() output)
+        #         c2 = make_divisible(min(c2, max_channels) * width, 8)
+
+            # c2 = ch[f] # 为了引用下SEAttention
             # args = [ch[f], *args]
         else:
             c2 = ch[f]
